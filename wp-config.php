e7dd0f6160a07cc0e5a1b7323faebce24e0d3cbb
Alexandra Barrera <selach231@gmail.com>
2024-04-02 07:53:52 -0500

    Se realizan algunas modificaciones

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'fundacion_angel_peludos' );
+define( 'DB_NAME', 'hogar_cai_dorada' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
