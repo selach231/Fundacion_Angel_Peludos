0fe0485d1fbe156fbb0cbc5f724949051ac17597
Alexandra Barrera <selach231@gmail.com>
2024-04-05 10:31:15 -0500

    Se realizan algunos cambios de configuracion de imagenes, instalacion de plugins y optimizacion del sitio

--- wp-content/wflogs/config-transient.php
+++ wp-content/wflogs/config-transient.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:12:{s:22:"attackDataNextInterval";N;s:16:"rulesLastUpdated";N;s:12:"premiumCount";N;s:12:"filePatterns";N;s:24:"filePatternCommonStrings";N;s:18:"filePatternIndexes";N;s:21:"signaturesLastUpdated";N;s:21:"signaturePremiumCount";N;s:23:"createInitialRulesDelay";i:1708795916;s:10:"watchedIPs";N;s:15:"blockedPrefixes";N;s:21:"blacklistAllowedCache";N;}
+a:13:{s:22:"attackDataNextInterval";N;s:16:"rulesLastUpdated";N;s:12:"premiumCount";N;s:12:"filePatterns";N;s:24:"filePatternCommonStrings";N;s:18:"filePatternIndexes";N;s:21:"signaturesLastUpdated";N;s:21:"signaturePremiumCount";N;s:23:"createInitialRulesDelay";i:1712330945;s:10:"watchedIPs";b:0;s:15:"blockedPrefixes";N;s:21:"blacklistAllowedCache";N;s:32:"cookieRedactionLastUpdateFailure";i:1712330648;}
