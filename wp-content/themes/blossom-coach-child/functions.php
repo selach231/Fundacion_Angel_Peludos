574fe97399d6fa8a876319709779cc16a97b8a70
Alexandra Barrera <selach231@gmail.com>
2024-04-05 10:43:11 -0500

    Se realizan algunas modificaciones en los plugins

--- wp-content/themes/blossom-coach-child/functions.php
+++ wp-content/themes/blossom-coach-child/functions.php
@@ -15,48 +15,6 @@ function child_theme_assets() {
 }
 add_action( 'wp_enqueue_scripts', 'child_theme_assets' );
 
-function auto_featured_image() {
-    global $post;
-   if (!has_post_thumbnail($post->ID)) {
-    $attached_image = get_children( "post_parent=$post->ID&amp;post_type=attachment&amp;post_mime_type=image&amp;numberposts=1" );
-   if ($attached_image) {
-   foreach ($attached_image as $attachment_id => $attachment) {
-   set_post_thumbnail($post->ID, $attachment_id);
-   }
-   }
-   }
-   }
-   add_action('the_post', 'auto_featured_image');
-   add_action('save_post', 'auto_featured_image');
-   add_action('draft_to_publish', 'auto_featured_image');
-   add_action('new_to_publish', 'auto_featured_image');
-   add_action('pending_to_publish', 'auto_featured_image');
-   add_action('future_to_publish', 'auto_featured_image');
-
-   add_filter( 'wp_insert_post_data', function ( $data, $postarr ) {
-    $post_id       = $postarr['ID'];
-    $post_status     = $data['post_status'];
-    $original_post_status = $postarr['original_post_status'];
-    if ( $post_id && 'publish' === $post_status && 'publish' !== $original_post_status ) {
-    $post_type = get_post_type( $post_id );
-    if ( post_type_supports( $post_type, 'thumbnail' ) && ! has_post_thumbnail( $post_id ) ) {
-    $data['post_status'] = 'draft';
-    }
-    }
-    return $data;
-    }, 10, 2 );
-    add_action( 'admin_notices', function () {
-    $post = get_post();
-    if ( 'publish' !== get_post_status( $post->ID ) && ! has_post_thumbnail( $post->ID ) ) { ?>
-    <div id="message" class="error">
-    <p>
-    <strong><?php _e( 'Porfavor agrega una imagen destacada o una imagen en el contexto de la entrada. Esta Noticia no podrá publicarse hasta que no agregues una imagen' ); ?></strong>
-    </p>
-    </div>
-    <?php
-    }
-    } );
-
     if ( !function_exists('AddThumbColumn') && function_exists('add_theme_support') ) {
         add_theme_support('post-thumbnails', array( 'post', 'page' ) );
         function AddThumbColumn($cols) {
